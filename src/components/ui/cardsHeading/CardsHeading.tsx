2d157e53174ee616f7d8c52f3801242f0d7a6012
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-03-09 23:19:58 +0900

    feat: 見出しコンポーネントを追加

---
+++ src/components/ui/cardsHeading/CardsHeading.tsx
@@ -0,0 +1,28 @@
+import { FC, ReactNode } from "react";
+
+import { WrapLink } from "@/components/feature/wrapLink";
+
+export type CardsHeadingProps = {
+  children: ReactNode;
+  href?: string;
+};
+
+export const CardsHeading: FC<CardsHeadingProps> = ({ children, href }) => {
+  const wrapped = (
+    <span
+      className={
+        "space-x-2 border-b-2 border-cyan-500 px-2 py-1 text-4xl font-bold"
+      }
+    >
+      {children}
+    </span>
+  );
+
+  return (
+    <header>
+      <h2 className={"p-2 text-center"}>
+        {href ? <WrapLink href={href}>{wrapped}</WrapLink> : wrapped}
+      </h2>
+    </header>
+  );
+};
