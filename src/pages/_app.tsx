13ced8dfcf816d8a8a73528c8dafe4ebc298e73a
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-03-19 00:11:12 +0900

    feat: notoSansJPをデフォルトのフォントにした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,29 @@
 import "@/styles/globals.css";
 
+import { Noto_Sans_JP } from "@next/font/google";
+import { FC, ReactNode } from "react";
+
 import type { AppProps } from "next/app";
 
 //ここに書いたものはstorybookでは読み込まれないので注意
 
+//normal: 400
+//bold: 700
+const notoSansJp = Noto_Sans_JP({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--noto-sans-jp",
+  display: "swap",
+});
+
+export const AppWrapper: FC<{ children: ReactNode }> = ({ children }) => {
+  return <div className={notoSansJp.className}>{children}</div>;
+};
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,9 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-blue-500 underline">
           Hello world!
         </h1>
+        <h2 className={"text-2xl"}>
+          人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは
+        </h2>
       </main>
     </>
   );
