31eeb58546010ec2393c9424743499d0bc73b6b9
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-03-18 17:37:44 +0900

    feat: WrapLinkでclassNameを受け取れるようにした

--- src/components/feature/wrapLink/WrapLink.tsx
+++ src/components/feature/wrapLink/WrapLink.tsx
@@ -4,8 +4,15 @@ import { FC, ReactNode } from "react";
 export type WrapLinkProps = {
   href: string;
   children: ReactNode;
+  className?: string;
 };
 
-export const WrapLink: FC<WrapLinkProps> = ({ href, children }) => {
-  return <Link href={href}>{children}</Link>;
+//classNameはNext/Linkコンポーネント内部のaタグに付与される
+
+export const WrapLink: FC<WrapLinkProps> = ({ href, children, className }) => {
+  return (
+    <Link href={href} className={className}>
+      {children}
+    </Link>
+  );
 };
