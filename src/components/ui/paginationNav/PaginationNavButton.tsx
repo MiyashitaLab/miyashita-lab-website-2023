31696bf5ddc05113ad78853ef1941e5ae3e41447
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-03-19 21:38:06 +0900

    fix: スタイル調整

--- src/components/ui/paginationNav/PaginationNavButton.tsx
+++ src/components/ui/paginationNav/PaginationNavButton.tsx
@@ -21,9 +21,10 @@ export const PaginationNavButton = ({
       href={href}
       aria-current={current ? "page" : undefined}
       className={classNames(
-        "block w-full text-center border border-gray-300 bg-white px-1 py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700",
+        "block w-full text-center border border-gray-300 bg-white px-1 py-2 leading-tight text-gray-500",
         className,
         {
+          "hover:bg-gray-100 hover:text-gray-700": href !== undefined,
           "text-cyan-700 bg-cyan-100 hover:text-cyan-800 hover:bg-cyan-200":
             current,
         }
