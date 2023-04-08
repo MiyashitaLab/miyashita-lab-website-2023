3000d70e60e2cc96f44040dac1aeb9bab9da1cb8
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-04-08 17:55:37 +0900

    feat: ArticleHeaderコンポーネントを実装

---
+++ src/components/ui/articleHeader/ArticleHeader.tsx
@@ -0,0 +1,39 @@
+import classNames from "classnames";
+import { ComponentProps, FC } from "react";
+
+import { BeautifulBreak } from "@/components/feature/beautifulBreak";
+import { dateToYYYYMMDD } from "@/lib/formatDate";
+
+export type ArticleHeaderProps = {
+  className?: ComponentProps<"div">["className"];
+  type: "news" | "project";
+  children: string;
+  date: Date;
+};
+
+export const ArticleHeader: FC<ArticleHeaderProps> = ({
+  className,
+  children,
+  type,
+  date,
+}) => {
+  return (
+    <div
+      className={classNames(
+        "w-full px-4 py-6 text-center text-white",
+        {
+          "bg-cyan-700": type === "news",
+          "bg-teal-700": type === "project",
+        },
+        className
+      )}
+    >
+      <h1 className={"text-xl font-bold md:text-2xl"}>
+        <BeautifulBreak>{children}</BeautifulBreak>
+      </h1>
+      <div className={"mt-2 text-sm"}>
+        <time>{dateToYYYYMMDD(date)}</time>
+      </div>
+    </div>
+  );
+};
