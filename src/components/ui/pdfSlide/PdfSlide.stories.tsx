e09e21e441ca5ce4b55acbfc814da7254e6bd54f
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-04-22 18:14:28 +0900

    feat: PDFスライドコンポーネントにコントロールバーを実装

--- src/components/ui/pdfSlide/PdfSlide.stories.tsx
+++ src/components/ui/pdfSlide/PdfSlide.stories.tsx
@@ -16,6 +16,6 @@ type Story = StoryObj<typeof PdfSlide>;
 export const Primary: Story = {
   args: {
     pdfUrl:
-      "https://storage.googleapis.com/miyashita-lab-website-2023-storage-dev/6563387d-7916-4f69-b573-fc6c75f61a8c%2F%E9%B9%BF%E5%B3%B6%2C%E5%8D%92%E8%AB%96%E8%AB%96%E6%96%872022%2C%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%88%E3%82%99.pdf",
+      "https://storage.googleapis.com/miyashita-lab-website-2023-storage-dev/83e3404f-b952-4a90-b692-8d6ab26ff08a/hci201-230116003808-1b702961.pdf",
   },
 };
--- src/components/ui/pdfSlide/PdfSlide.tsx
+++ src/components/ui/pdfSlide/PdfSlide.tsx
@@ -1,6 +1,18 @@
-import { FC } from "react";
+import classNames from "classnames";
+import {
+  createRef,
+  FC,
+  RefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
+import { SlideControlBar } from "@/components/ui/pdfSlide/SlideControlBar";
+
+// /publicに置いても良いけどめんどかったので
 const cMapUrl = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`;
 const pdfJsWorker = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -8,24 +20,137 @@ pdfjs.GlobalWorkerOptions.workerSrc = pdfJsWorker;
 
 export type PdfSlideProps = {
   pdfUrl: string;
+  width: number;
+  height: number;
 };
 
-export const PdfSlide: FC<PdfSlideProps> = ({ pdfUrl }) => {
+export const PdfSlide: FC<PdfSlideProps> = ({ pdfUrl, height, width }) => {
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [numPages, setNumPages] = useState<number>(0);
+  const [initialPageLoaded, setInitialPageLoaded] = useState<boolean>(false);
+
+  const pageRefs = useRef<RefObject<HTMLCanvasElement>[]>([]);
+  Array.from(new Array(numPages), (_, i) => i).forEach((i) => {
+    pageRefs.current[i] = createRef<HTMLCanvasElement>();
+  });
+
+  const turnToPrevPage = useCallback(() => {
+    setCurrentPage((prev) => Math.max(prev - 1, 0));
+  }, []);
+
+  const turnToNextPage = useCallback(() => {
+    setCurrentPage((prev) => Math.min(prev + 1, numPages - 1));
+  }, [numPages]);
+
+  const turnToFirstPage = useCallback(() => {
+    setCurrentPage(0);
+  }, []);
+
+  const turnToLastPage = useCallback(() => {
+    setCurrentPage(numPages - 1);
+  }, [numPages]);
+
+  //canvasがロードされてからじゃないとrefが取れない
+  useEffect(() => {
+    if (!initialPageLoaded) return;
+    const canvas = pageRefs.current[currentPage]?.current;
+
+    const calcCursorArea = (
+      e: MouseEvent
+    ): "left" | "right" | "center" | "out" => {
+      const rect = canvas?.getBoundingClientRect();
+      if (!rect) return "out";
+      if (e.clientX - rect.left < rect.width / 3) {
+        return "left";
+      }
+      if ((rect.width / 3) * 2 < e.clientX - rect.left) {
+        return "right";
+      }
+      return "center";
+    };
+
+    const handleClick = (e: MouseEvent) => {
+      const area = calcCursorArea(e);
+      if (area === "left") {
+        turnToPrevPage();
+      }
+      if (area === "right") {
+        turnToNextPage();
+      }
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (canvas === null) return;
+      const area = calcCursorArea(e);
+      if (area === "left" || area === "right") {
+        canvas.style.cursor = "pointer";
+      } else {
+        canvas.style.cursor = "default";
+      }
+    };
+
+    canvas?.addEventListener("click", handleClick);
+    canvas?.addEventListener("mousemove", handleMouseMove);
+    // ページ切り替え後にもポインタを変えたいのでmouseenterでやる
+    canvas?.addEventListener("mouseenter", handleMouseMove);
+    return () => {
+      canvas?.removeEventListener("click", handleClick);
+      canvas?.removeEventListener("mousemove", handleMouseMove);
+      canvas?.removeEventListener("mouseenter", handleMouseMove);
+    };
+  }, [
+    initialPageLoaded,
+    pageRefs,
+    currentPage,
+    turnToNextPage,
+    turnToPrevPage,
+  ]);
+
   return (
-    <div>
+    <div className={"max-w-lg"}>
       <Document
         file={pdfUrl}
         options={{
           cMapUrl: cMapUrl,
           cMapPacked: true,
         }}
+        onLoadSuccess={({ numPages }) => {
+          console.log("numPages", numPages);
+          setNumPages(numPages);
+        }}
       >
-        <Page
-          pageNumber={2}
-          renderTextLayer={false}
-          renderAnnotationLayer={false}
-        />
+        {Array.from(new Array(numPages), (_, index) => {
+          // 2ページ以上離れている場合は非表示にする
+          // 全ページ見たら結局変わらないが、初回ロード時のメモリ節約にはなるはず
+          if (2 < Math.abs(currentPage - index)) return undefined;
+          const width = 512;
+          return (
+            <Page
+              key={index}
+              className={classNames({
+                hidden: index !== currentPage,
+              })}
+              canvasRef={pageRefs.current[index]}
+              width={width}
+              pageIndex={index}
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              onRenderSuccess={() => {
+                setInitialPageLoaded(true);
+              }}
+            />
+          );
+        })}
       </Document>
+      <SlideControlBar
+        currentPage={currentPage}
+        numPages={numPages}
+        turnToPrevPage={turnToPrevPage}
+        turnToNextPage={turnToNextPage}
+        turnToFirstPage={turnToFirstPage}
+        turnToLastPage={turnToLastPage}
+        pdfDownloadUrl={pdfUrl}
+      />
     </div>
   );
 };
