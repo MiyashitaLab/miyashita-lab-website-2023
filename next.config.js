e5fddbd2af74f8593d6b1406a7d39f2930d4564e
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-05-06 01:23:14 +0900

    feat: contentfulの画像ファイルをnext/imageで使用できるように

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.ctfassets.net",
+        protocol: "https",
+        pathname: "/srfcax7yjz0v/**", //TODO contentfulのスペースIDが変わったら変更する
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
