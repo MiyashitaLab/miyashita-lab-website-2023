a5f77db3fbae906d965f91753bb634d240c01862
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-05-07 19:13:11 +0900

    feat: WrapImageでmaxWidthの代わりにsizesを指定できるようにした

--- src/components/feature/wrapImage/WrapImage.tsx
+++ src/components/feature/wrapImage/WrapImage.tsx
@@ -1,30 +1,70 @@
 import Image from "next/image";
-import { FC } from "react";
+import { CSSProperties, FC } from "react";
 
-export type WrapImageProps = {
+export type CMSImage = {
   src: string;
-  alt: string;
   originalWidth: number;
   originalHeight: number;
-  maxWidth: number;
-  maxHeight: number;
 };
+
+export type WrapImageProps = (CMSImage & {
+  alt: string;
+}) &
+  (
+    | {
+        maxWidth: number;
+        maxHeight: number;
+      }
+    | {
+        sizes: {
+          base: Exclude<CSSProperties["width"], undefined>;
+          sm?: CSSProperties["width"]; // 640pxまで
+          md?: CSSProperties["width"]; // 768pxまで
+          lg?: CSSProperties["width"]; // 1024pxまで
+          xl?: CSSProperties["width"]; // 1280pxまで
+        };
+      }
+  );
 
 export const WrapImage: FC<WrapImageProps> = ({
   src,
   alt,
   originalWidth,
   originalHeight,
-  maxWidth,
-  maxHeight,
+  ...rest
 }) => {
-  const { width, height } = calcImgSize(
-    maxWidth,
-    maxHeight,
-    originalWidth,
-    originalHeight
-  );
-  return <Image src={src} alt={alt} width={width} height={height} />;
+  if ("sizes" in rest) {
+    const { base, sm, md, lg, xl } = rest.sizes;
+    const sizes = [
+      { minWidth: 1280, size: xl },
+      { minWidth: 1024, size: lg },
+      { minWidth: 768, size: md },
+      { minWidth: 640, size: sm },
+    ]
+      .filter(({ size }) => size !== undefined)
+      .map(({ minWidth, size }) => `(min-width: ${minWidth}) ${size}`)
+      .concat(`${base}`)
+      .join(", ");
+
+    return (
+      <Image
+        src={src}
+        alt={alt}
+        width={originalWidth}
+        height={originalHeight}
+        sizes={sizes}
+      />
+    );
+  } else {
+    const { maxWidth, maxHeight } = rest;
+    const { width, height } = calcImgSize(
+      maxWidth,
+      maxHeight,
+      originalWidth,
+      originalHeight
+    );
+    return <Image src={src} alt={alt} width={width} height={height} />;
+  }
 };
 
 const calcImgSize = (
