1cf3697250fa254799dd5278d837ead0fee75eef
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-05-20 17:50:43 +0900

    feat: メンバー一覧ページのページコンポーネントを実装

---
+++ src/components/page/members/Members.stories.tsx
@@ -0,0 +1,25 @@
+import { StoryObj } from "@storybook/react";
+
+import { Members } from "./Members";
+
+import { Primary as TopPrimary } from "@/components/page/top/Top.stories";
+
+import type { Meta } from "@storybook/react";
+
+const meta: Meta<typeof Members> = {
+  title: "Pages/Members",
+  component: Members,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Members>;
+
+export const Primary: Story = {
+  args: {
+    enrolledCardsHeading: "現役メンバー",
+    enrolledCards: TopPrimary.args.member.cards,
+    graduatedCardsHeading: "歴代メンバー",
+    graduatedCards: TopPrimary.args.member.cards,
+  },
+};
