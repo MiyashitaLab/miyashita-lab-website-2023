cec2120a41d3e0bd456ae8b7e2e1ce0f219f13ab
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-05-27 14:22:03 +0900

    feat: ArticleCardにラベルと追加説明を置けるようにした

--- src/components/ui/articleCard/ArticleCard.stories.tsx
+++ src/components/ui/articleCard/ArticleCard.stories.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 
 import { ArticleCard } from "./ArticleCard";
 
+import { ArticleCardDescription } from "@/components/ui/articleCardDescription";
+import { Icon } from "@/components/ui/icon";
+import { Label } from "@/components/ui/label";
+
 import type { Meta } from "@storybook/react";
 
 const meta: Meta<typeof ArticleCard> = {
@@ -49,5 +53,39 @@ export const LongTitle: Story = {
       </>
     ),
     title: "オープンラボ・全方位進路相談会を開催します！（2022年度）",
+  },
+};
+
+export const Research: Story = {
+  args: {
+    className: "w-96",
+    date: new Date("2021-12-31"),
+    href: "#",
+    children: (
+      <>
+        <Image
+          src="https://i.gyazo.com/fb446d3a093d2916907e1ee38ab7907a.png"
+          alt={"test image"}
+          width={384}
+          height={216}
+        />
+      </>
+    ),
+    title: "オープンラボ・全方位進路相談会を開催します！（2022年度）",
+    label: <Label className={"bg-cyan-700"}>研究報告</Label>,
+    description: (
+      <ArticleCardDescription
+        items={[
+          {
+            icon: <Icon fontStyle={"solid"} name={"user"} />,
+            text: "Alice / Bob / Carol",
+          },
+          {
+            icon: <Icon fontStyle={"solid"} name={"book"} />,
+            text: "なんとか処理学会",
+          },
+        ]}
+      />
+    ),
   },
 };
