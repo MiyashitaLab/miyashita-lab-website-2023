6ac70734c5c39eb6191652b8e38b07c5d775447b
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-05-28 19:16:51 +0900

    feat: ResearchHeroを実装

---
+++ src/components/page/research/ResearchHero.tsx
@@ -0,0 +1,68 @@
+import { FC } from "react";
+
+import { CMSImage, WrapImage } from "@/components/feature/wrapImage/WrapImage";
+import { PdfSlide } from "@/components/ui/pdfSlide";
+
+export type ResearchHeroProps = {
+  hero:
+    | {
+        type: "image";
+        image: CMSImage;
+      }
+    | {
+        type: "youtube";
+        youtubeUrl: string;
+      }
+    | {
+        type: "slide";
+        slidePdfUrl: string;
+      };
+};
+
+export const ResearchHero: FC<ResearchHeroProps> = ({ hero }) => {
+  if (hero.type === "image") {
+    return (
+      <div className={"bg-stone-100"}>
+        <WrapImage
+          className={"mx-auto"}
+          src={hero.image.src}
+          originalWidth={hero.image.originalWidth}
+          originalHeight={hero.image.originalHeight}
+          maxHeight={384}
+          maxWidth={9999}
+          alt={""}
+        />
+      </div>
+    );
+  }
+
+  if (hero.type === "youtube") {
+    const youtubeId = new URL(hero.youtubeUrl).searchParams.get("v");
+    if (youtubeId === null) return <></>;
+
+    //TODO widget_referrerを設定する
+    return (
+      <div className={"bg-stone-100"}>
+        <iframe
+          className={"mx-auto"}
+          width="682"
+          height="384"
+          src={`https://www.youtube-nocookie.com/embed/${youtubeId}?modestbranding=1&rel=0`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </div>
+    );
+  }
+
+  if (hero.type === "slide") {
+    return (
+      <div className={"bg-gray-100"}>
+        <PdfSlide width={"full"} height={384} pdfUrl={hero.slidePdfUrl} />
+      </div>
+    );
+  }
+
+  return <></>;
+};
