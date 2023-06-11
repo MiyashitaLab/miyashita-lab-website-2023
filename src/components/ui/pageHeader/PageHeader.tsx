1987d746d7846b39a5de82f0c9bf0c69604b1a59
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-06-11 17:00:21 +0900

    refactor: PageHeaderからclassname Propを削除

--- src/components/ui/pageHeader/PageHeader.tsx
+++ src/components/ui/pageHeader/PageHeader.tsx
@@ -11,7 +11,6 @@ export type PageHeaderLinkItem = {
 };
 
 export type PageHeaderProps = {
-  className?: string;
   logo: ReactElement;
   logoHref: string;
   links: readonly PageHeaderLinkItem[];
@@ -19,12 +18,7 @@ export type PageHeaderProps = {
 
 //md未満はハンバーガーメニューにする
 
-export const PageHeader: FC<PageHeaderProps> = ({
-  className,
-  logo,
-  logoHref,
-  links,
-}) => {
+export const PageHeader: FC<PageHeaderProps> = ({ logo, logoHref, links }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClickHamburger = () => {
@@ -32,52 +26,51 @@ export const PageHeader: FC<PageHeaderProps> = ({
   };
 
   return (
-    <header className={classNames(className)}>
-      <nav className="border-gray-200 bg-white px-4">
-        <div className="mx-auto flex h-full max-w-screen-xl flex-wrap items-center justify-between bg-white py-1">
-          <div className={"flex h-10 items-center"}>
-            <WrapLink href={logoHref}>{logo}</WrapLink>
-          </div>
+    <header className={"w-full bg-white px-4"}>
+      <nav className="mx-auto flex h-full max-w-screen-xl flex-wrap items-center justify-between bg-white py-1">
+        <div className={"flex h-10 items-center"}>
+          <WrapLink href={logoHref}>{logo}</WrapLink>
+        </div>
 
-          <div className={"block md:hidden"}>
-            <button
-              className={"h-10 w-10 rounded-lg text-gray-500 hover:bg-gray-200"}
-              onClick={handleClickHamburger}
-            >
-              <Icon size={"lg"} fontStyle={"solid"} name={"bars"} />
-            </button>
-          </div>
-          <div
-            className={classNames("w-full md:flex md:w-auto pt-2 md:pt-0", {
-              hidden: !isOpen,
-            })}
+        {/* ハンバーガーボタン */}
+        <div className={"block md:hidden"}>
+          <button
+            className={"h-10 w-10 rounded-lg text-gray-500 hover:bg-gray-200"}
+            onClick={handleClickHamburger}
           >
-            <ul className={"flex flex-col md:flex-row md:space-x-4"}>
-              {links.map(({ text, href, highlight }) => (
-                <li key={href}>
-                  <WrapLink href={href}>
-                    <div
-                      className={classNames(
-                        "block rounded text-md py-1 pl-2",
-                        "border-b border-gray-100",
-                        "md:border-b-0",
-                        {
-                          "text-white bg-teal-500 md:text-cyan-500 md:bg-transparent":
-                            highlight,
-                        },
-                        {
-                          "text-gray-700 hover:bg-gray-100 md:hover:underline decoration-cyan-500 decoration-1 underline-offset-4 md:hover:bg-transparent":
-                            !highlight,
-                        }
-                      )}
-                    >
-                      {text}
-                    </div>
-                  </WrapLink>
-                </li>
-              ))}
-            </ul>
-          </div>
+            <Icon size={"lg"} fontStyle={"solid"} name={"bars"} />
+          </button>
+        </div>
+        <div
+          className={classNames("w-full md:flex md:w-auto pt-2 md:pt-0", {
+            hidden: !isOpen,
+          })}
+        >
+          <ul className={"flex flex-col md:flex-row md:space-x-4"}>
+            {links.map(({ text, href, highlight }) => (
+              <li key={href}>
+                <WrapLink href={href}>
+                  <div
+                    className={classNames(
+                      "block rounded text-md py-1 pl-2",
+                      "border-b border-gray-100",
+                      "md:border-b-0",
+                      {
+                        "text-white bg-teal-500 md:text-cyan-500 md:bg-transparent":
+                          highlight,
+                      },
+                      {
+                        "text-gray-700 hover:bg-gray-100 md:hover:underline decoration-cyan-500 decoration-1 underline-offset-4 md:hover:bg-transparent":
+                          !highlight,
+                      }
+                    )}
+                  >
+                    {text}
+                  </div>
+                </WrapLink>
+              </li>
+            ))}
+          </ul>
         </div>
       </nav>
     </header>
