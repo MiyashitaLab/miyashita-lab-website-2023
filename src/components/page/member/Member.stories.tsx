8eb23cbcd49865bd73f04d1ac5c61b25dc2da194
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-06-11 18:08:01 +0900

    feat: contentを分割して、所属を追加し、リファクタリングをした

--- src/components/page/member/Member.stories.tsx
+++ src/components/page/member/Member.stories.tsx
@@ -18,8 +18,6 @@ type Story = StoryObj<typeof Member>;
 const content = `
 Homei Miyashita, Ph.D.
 
-**明治大学 総合数理学部 先端メディアサイエンス学科 教授 / JST CREST**
-
 Homei Miyashita is a professor in the School of Science and Technology at Meiji University.
 
 Born in Firenze, Italy, in 1976, he received a bachelor’s degree with a major in Image Science from Chiba University in 2001, a master’s degree with a major in Music Composition from Toyama University in 2003, and a Ph.D. degree with a major in Knowledge Science from Japan Advanced Institute of Science and Technology (JAIST) in 2006.
@@ -27,14 +25,23 @@ Born in Firenze, Italy, in 1976, he received a bachelor’s degree with a major 
 His general research interests include entertainment computing and human-computer interaction.
 `;
 
+const achievements = `
+- 業績1
+- 業績2
+- 業績3
+`;
+
 export const Primary: Story = {
   args: {
     name: "宮下芳明",
     role: "教員",
+    institution:
+      "明治大学 総合数理学部 先端メディアサイエンス学科 教授 / JST CREST",
     thumbnail: {
       src: "./member-default.png",
     },
-    content: content,
+    introductionMarkdownContent: content,
+    achievementsMarkdownContent: achievements,
     researchesUrl: "#",
   },
   render: (args) => (
--- src/components/page/member/Member.tsx
+++ src/components/page/member/Member.tsx
@@ -7,41 +7,69 @@ import { PageLink } from "@/components/ui/pageLink";
 export type MemberProps = {
   name: string;
   role: string;
+  institution: string;
   thumbnail: CMSImage;
-  content: string;
+  introductionMarkdownContent: string;
+  achievementsMarkdownContent: string;
   researchesUrl?: string;
 };
 
 export const Member: FC<MemberProps> = ({
   name,
   role,
+  institution,
   thumbnail,
-  content,
+  introductionMarkdownContent,
+  achievementsMarkdownContent,
   researchesUrl,
 }) => {
-  return (
-    <div className={"m-4 flex flex-col md:flex-row"}>
-      <div className={"mx-auto shrink-0 p-4"}>
+  const renderPageLink = () => {
+    return researchesUrl && <PageLink href={researchesUrl}>論文一覧</PageLink>;
+  };
+
+  const renderImgArea = () => {
+    return (
+      <div className={"flex flex-col items-center"}>
         <WrapImageSized src={thumbnail.src} alt={""} width={200} height={200} />
-        {researchesUrl && (
-          <PageLink
-            href={researchesUrl}
-            className={"hidden py-4 text-center text-lg md:block"}
-          >
-            論文一覧
-          </PageLink>
-        )}
+        <div className={"hidden py-4 text-center text-lg md:block"}>
+          {renderPageLink()}
+        </div>
       </div>
-      <div className={"p-4"}>
+    );
+  };
+
+  const renderIntroductionArea = () => {
+    return (
+      <>
         <h1 className={"text-3xl font-semibold"}>{name}</h1>
         <p className={"my-2"}>{role}</p>
-        {researchesUrl && (
-          <PageLink href={researchesUrl} className={"md:hidden"}>
-            論文一覧
-          </PageLink>
-        )}
-        <hr className={"my-4"} />
-        <MarkdownContent markdown={content} />
+        <p className={"my-2"}>{institution}</p>
+        <div className={"md:hidden"}>{renderPageLink()}</div>
+      </>
+    );
+  };
+
+  //grid-template-areasめちゃくちゃ使いたい
+
+  return (
+    <div
+      className={
+        "grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto_auto_1fr] gap-x-8 gap-y-4 p-6"
+      }
+    >
+      <div className={"col-span-2 row-span-1 md:col-span-1 md:row-span-3"}>
+        {renderImgArea()}
+      </div>
+      <div className={"col-span-2 row-span-1 md:col-span-1 md:row-span-1"}>
+        {renderIntroductionArea()}
+      </div>
+      <hr className={"col-span-2 row-span-1 md:col-span-1 md:row-span-1"} />
+      <div className={"col-span-2 row-span-1 md:col-span-1 md:row-span-1"}>
+        <MarkdownContent markdown={introductionMarkdownContent} />
+      </div>
+      <hr className={"col-span-2 row-span-1 md:row-span-1"} />
+      <div className={"col-span-2 row-span-1 md:row-span-1"}>
+        <MarkdownContent markdown={achievementsMarkdownContent} />
       </div>
     </div>
   );
