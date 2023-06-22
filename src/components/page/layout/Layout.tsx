e7ca566713facc1f3f35dc12e94a02b09861d537
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-06-23 02:46:24 +0900

    fix: ヘッダーロゴのリンクが/になっていなかったのを修正

--- src/components/page/layout/Layout.tsx
+++ src/components/page/layout/Layout.tsx
@@ -91,7 +91,7 @@ export const Layout: FC<LayoutProps> = ({
               height={32}
             />
           }
-          logoHref={"#home"}
+          logoHref={"/"}
           links={links}
         />
       </div>
