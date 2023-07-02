0e4845731c172437f53b99ee90d8cf64cda5cf9b
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-07-02 18:46:55 +0900

    feat: revalidate時のslack通知機能を実装

--- src/pages/api/isr.ts
+++ src/pages/api/isr.ts
@@ -2,6 +2,12 @@ import { NextApiHandler } from "next";
 
 import { modelDependencies } from "@/lib/cms/modelDependencies";
 
+type RevalidateResult = {
+  route: string;
+  success: boolean;
+  error?: string;
+};
+
 const handler: NextApiHandler = async (req, res) => {
   const secret = req.query.secret ?? req.body.secret;
 
@@ -36,18 +42,27 @@ const handler: NextApiHandler = async (req, res) => {
         return {
           route: route,
           success: true,
-        };
+        } satisfies RevalidateResult;
       } catch (e) {
         return {
           route: route,
           success: false,
           error: String(e),
-        };
+        } satisfies RevalidateResult;
       }
     })
   );
 
   res.status(200).json({ revalidateResults });
+
+  if (process.env.SLACK_WEBHOOK_URL) {
+    await noticeToSlack(process.env.SLACK_WEBHOOK_URL, revalidateResults).catch(
+      (error) => {
+        console.warn("Slack通知に失敗しました", error);
+        // ignore
+      }
+    );
+  }
 };
 
 const validModelQueryValue = Object.keys(modelDependencies);
@@ -55,5 +70,42 @@ const validateModelQueryValue = (
   model: string
 ): model is keyof typeof modelDependencies =>
   validModelQueryValue.includes(model);
+
+const noticeToSlack = async (webhook: string, results: RevalidateResult[]) => {
+  const body = {
+    blocks: [
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `以下のページが更新されます（${new Date().toLocaleString(
+            "ja"
+          )}）`,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: results
+            .map((result) =>
+              result.success
+                ? `✅ \`${result.route}\``
+                : `❌ \`${result.route}\`\nerror: ${result.error}`
+            )
+            .join("\n"),
+        },
+      },
+    ],
+  };
+
+  const res = await fetch(webhook, {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+};
 
 export default handler;
