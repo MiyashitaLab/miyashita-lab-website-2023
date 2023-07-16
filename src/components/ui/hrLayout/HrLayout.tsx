5b8682e71a873a6b4c0e3c030cc6f3ba0cb47502
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-07-16 17:43:28 +0900

    refactor: hrで区切る部分をコンポーネント化

---
+++ src/components/ui/hrLayout/HrLayout.tsx
@@ -0,0 +1,28 @@
+import { FC, ReactElement, ReactNode } from "react";
+
+export type HrLayoutProps = {
+  children: ReactNode[];
+  hr: ReactElement;
+};
+
+/**
+ * 与えられたchildrenの間にhrを挟む
+ * @param children
+ * @param hr hrの要素
+ */
+export const HrLayout: FC<HrLayoutProps> = ({ children, hr }) => {
+  const validChildren = children.filter(
+    (child) => child !== null && child !== undefined
+  );
+
+  return (
+    <>
+      {validChildren.map((child, index) => (
+        <>
+          {child}
+          {index !== validChildren.length - 1 && hr}
+        </>
+      ))}
+    </>
+  );
+};
