08ca904f9e4c64fdd2c1bc5891f299689eacfa14
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-07-17 17:43:15 +0900

    feat: media APIを生やした

---
+++ src/pages/api/media.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "node:stream";
+
+import { client } from "@/lib/cms/contentfulClient";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const assetId = req.query.id as string;
+
+  if (!assetId) {
+    return res.status(400).json({ message: "id is required" });
+  }
+
+  const asset = await client.getAsset(assetId);
+  const assetUrl = asset.fields.file?.url;
+
+  if (!assetUrl) {
+    return res.status(404).json({ message: "asset not found" });
+  }
+
+  const assetRes = await fetch(`https:${assetUrl}`);
+
+  if (!assetRes.ok || !assetRes.body) {
+    res.status(404).json({ message: "asset not found" });
+    return;
+  }
+
+  //Vercelの仕様で最大でも1ヶ月
+  //7日
+  res.setHeader("Cache-Control", "s-maxage=604800");
+  res.setHeader("Content-type", "application/pdf");
+
+  //WHATWGのfetchで得られるReadableStreamはNode.jsのinternal.Readableとは別物なので、そのままではpipeできない
+  const reader = assetRes.body.getReader();
+  const stream = new Readable({
+    read() {
+      reader.read().then(({ done, value }) => {
+        if (done) {
+          this.push(null);
+        } else {
+          this.push(value);
+        }
+      });
+    },
+  });
+  stream.pipe(res);
+};
+
+export default handler;
