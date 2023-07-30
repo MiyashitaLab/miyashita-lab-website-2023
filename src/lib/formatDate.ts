a2df4c0cdb384bb6682217017f786edc7494bfe9
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-07-30 17:38:47 +0900

    feat: formatDateのseparatorを指定できるようにした

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,7 +1,7 @@
-export const dateToYYYYMMDD = (date: Date): string => {
+export const dateToYYYYMMDD = (date: Date, separator = "-"): string => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
 
-  return `${year}-${month}-${day}`;
+  return [year, month, day].join(separator);
 };
